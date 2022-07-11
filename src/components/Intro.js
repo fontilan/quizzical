95cfc1a12d668cb2ba2285e71935b3bfb5faec6b
fontilan <68748054+fontilan@users.noreply.github.com>
2022-07-11 19:58:09 +0200

    add intro Component

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,8 +1,8 @@
-const Intro = ({ startGame }) => (
+const Intro = ({ onClick }) => (
   <div className="intro">
     <h1 className="intro--title">Quizzical</h1>
     <p className="intro--description">See how good you are with trivia!</p>
-    <button className="intro--button" onClick={startGame}>
+    <button className="intro--button" onClick={onClick}>
       Start quiz
     </button>
   </div>
