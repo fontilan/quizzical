58fe32eed85327cfb41ac003b5cd1ba5f70935d8
fontilan <68748054+fontilan@users.noreply.github.com>
2023-01-02 15:57:34 +0100

    refactor: button text logic

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,17 +1,6 @@
 import React from 'react';
 
 const Summary = ({ confirm, gameEnded, numberOfQuestions, points }) => {
-  const Button = () => {
-    let buttonText;
-    gameEnded ? (buttonText = 'Play again') : (buttonText = 'Check answers');
-
-    return (
-      <button className="summary--button" onClick={confirm} type="button">
-        {buttonText}
-      </button>
-    );
-  };
-
   return (
     <div className="summary">
       {gameEnded && (
@@ -19,7 +8,9 @@ const Summary = ({ confirm, gameEnded, numberOfQuestions, points }) => {
           You scored {points}/{numberOfQuestions} correct answers
         </p>
       )}
-      <Button />
+      <button className="summary--button" onClick={confirm} type="button">
+        {`${gameEnded ? `Play again` : 'Check answers'}`}
+      </button>
     </div>
   );
 };
