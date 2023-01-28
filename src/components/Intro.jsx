16c6e568e2396271db4108985d55b7963a4569c4
fontilan <68748054+fontilan@users.noreply.github.com>
2023-01-28 22:39:50 +0100

    init

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/prop-types */
 import React from 'react';
 
@@ -6,9 +7,30 @@ function Intro({ onClick }) {
     <div className="intro">
       <h1 className="intro--title">Quizzical</h1>
       <p className="intro--description">See how good you are with trivia!</p>
-      <button type="button" className="intro--button" onClick={onClick}>
-        Start quiz
-      </button>
+      <form>
+        <label>
+          Select Category:
+          <select>
+            <option value="any">Any Category</option>
+            <option value="e-books">Entertainment: Books</option>
+            <option value="e-film">Entertainment: Film</option>
+            <option value="e-music">Entertainment: Music</option>
+            <option value="general">General Knowledge</option>
+          </select>
+        </label>
+        <label>
+          Difficulty
+          <select>
+            <option value="any">Any difficulty</option>
+            <option value="easy">Easy</option>
+            <option value="medium">Medium</option>
+            <option value="hard">Hard</option>
+          </select>
+        </label>
+        <button type="button" className="intro--button" onClick={onClick}>
+          Start quiz
+        </button>
+      </form>
     </div>
   );
 }
