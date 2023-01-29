518db1a125eb77bce6b2a1d761247068d455e844
fontilan <68748054+fontilan@users.noreply.github.com>
2023-01-29 13:35:46 +0100

    feat: get form data

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,14 +3,29 @@
 import React from 'react';
 
 function Intro({ onClick }) {
+  function handleSubmit(e) {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    // option 1
+    console.log(new URLSearchParams(formData).toString());
+    // option 2
+    const formJSON = Object.fromEntries(formData.entries());
+    console.log(formJSON);
+    // option 3
+    console.log([...formData.entries()]);
+
+    onClick();
+  }
+
   return (
     <div className="intro">
       <h1 className="intro--title">Quizzical</h1>
       <p className="intro--description">See how good you are with trivia!</p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           Select Category:
-          <select>
+          <select name="selectedCategory">
             <option value="any">Any Category</option>
             <option value="e-books">Entertainment: Books</option>
             <option value="e-film">Entertainment: Film</option>
@@ -20,14 +35,14 @@ function Intro({ onClick }) {
         </label>
         <label>
           Difficulty
-          <select>
-            <option value="any">Any difficulty</option>
+          <select name="selectedDifficulty">
+            <option value="any">Any Difficulty</option>
             <option value="easy">Easy</option>
             <option value="medium">Medium</option>
             <option value="hard">Hard</option>
           </select>
         </label>
-        <button type="button" className="intro--button" onClick={onClick}>
+        <button type="submit" className="intro--button">
           Start quiz
         </button>
       </form>
