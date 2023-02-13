94408c4da388d4b840ed82794a7ec00703b25541
fontilan <68748054+fontilan@users.noreply.github.com>
2023-02-13 09:38:59 +0100

    feat: hardcode the number of questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
 import React, { useState } from 'react';
-
 import { nanoid } from 'nanoid';
 import Intro from './components/Intro';
 import Questions from './components/Questions';
 import Summary from './components/Summary';
 
 function App() {
+  const [category, setCategory] = useState(9);
   const [currentQuestions, setCurrentQuestions] = useState();
   const [gameEnded, setGameEnded] = useState(false);
   const [gameStarted, setGameStarted] = useState(false);
   const [points, setPoints] = useState(0);
-  const [category, setCategory] = useState(9);
-
-  const numberOfQuestions = 5;
 
   const fetchQuestions = async () => {
     const response = await fetch(
-      `https://opentdb.com/api.php?amount=${numberOfQuestions}&category=${category}&type=multiple&encode=url3986`,
+      `https://opentdb.com/api.php?amount=5&category=${category}&type=multiple&encode=url3986`,
     );
 
     const data = await response.json();
@@ -91,7 +88,6 @@ function App() {
           <Summary
             gameEnded={gameEnded}
             handleClick={handleClick}
-            numberOfQuestions={numberOfQuestions}
             points={points}
           />
         </>
--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,13 +1,11 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 
-function Summary({ handleClick, gameEnded, numberOfQuestions, points }) {
+function Summary({ handleClick, gameEnded, points }) {
   return (
     <div className="summary">
       {gameEnded && (
-        <p className="summary--text">
-          You scored {points}/{numberOfQuestions} correct answers
-        </p>
+        <p className="summary--text">You scored {points}/5 correct answers</p>
       )}
       <button className="summary--button" onClick={handleClick} type="button">
         {`${gameEnded ? `Play again` : 'Check answers'}`}
